62111c88aa782604b26a85667d18b3e09a903b9e
bryansilvagit <basp.95@outlook.com>
2021-11-29 11:45:49 -0500

    Add CI/CD impl

--- src/main/java/com/javeriana/ares/sds/resolutions/entrypoint/service/ResolutionServiceImpl.java
+++ src/main/java/com/javeriana/ares/sds/resolutions/entrypoint/service/ResolutionServiceImpl.java
@@ -6,6 +6,8 @@ import com.javeriana.ares.sds.resolutions.crosscutting.constants.Constants;
 import com.javeriana.ares.sds.resolutions.model.domain.ResolutionDO;
 import com.javeriana.ares.sds.resolutions.model.template.ResolutionTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,6 +18,7 @@ import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.UUID;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class ResolutionServiceImpl {
@@ -27,9 +30,11 @@ public class ResolutionServiceImpl {
 
     @KafkaListener(topics = Constants.TOPIC_NAME)
     public void createResolution(String message) {
+        log.info("Message received....");
         JsonObject response = new Gson().fromJson(message, JsonObject.class);
         String statusValue = response.get("estado").getAsString();
         if (statusValue.equalsIgnoreCase(Constants.STATUS_APPROVE) || statusValue.equalsIgnoreCase(Constants.STATUS_DENY)) {
+            log.info("Reading message with status " + statusValue);
             JsonObject user = response.getAsJsonObject("creador");
             JsonObject data = response.getAsJsonObject("data");
             boolean status = statusValue.equalsIgnoreCase(Constants.STATUS_APPROVE);
@@ -47,6 +52,7 @@ public class ResolutionServiceImpl {
                     .build();
             ResolutionTemplate.createResolution(resolutionFile, resolutionDO);
             String newMessage = message.replace(statusValue, statusValue.toUpperCase() + Constants.STATUS_GENERATED);
+            log.info("Sending message...." + newMessage);
             kafkaTemplate.send(Constants.TOPIC_NAME, UUID.randomUUID().toString(), newMessage);
         }
     }
